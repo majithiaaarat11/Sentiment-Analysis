3aba00e46c713f9f2257db72dccb328d6c07e1e3
majithiaaarat11 <36953697+majithiaaarat11@users.noreply.github.com>
2018-08-22 09:40:17 +0530

    Update NBC(1).py

--- NBC(1).py
+++ NBC(1).py
@@ -16,7 +16,7 @@ from sklearn.metrics import roc_auc_score
 from sklearn.metrics import confusion_matrix
 import numpy as np
 
-df = pd.read_csv("training.txt",sep="\t",names=['liked','review'])
+df = pd.read_csv("https://storage.googleapis.com/kaggle-competitions-data/kaggle/2558/training.txt?GoogleAccessId=web-data@kaggle-161607.iam.gserviceaccount.com&Expires=1535135533&Signature=MuaM3iscrkhpruPU6OlyVPn1LOG9Gbj8C4%2FfPcsR76XDl3O9rB5DnpONGXs3gl0aUNbfkWDXSORryNPUKPVa5ky2gow5cdNVuSf%2B%2BfgOv1LKn1eJRiLYzmMD%2FdAYn6ksj4Dw0tlS6XozhtsMjjSO6%2FthcxyIoYHqyXIpkv6B7iZ6bBmoVqXOTlxL27y3ej965R9QBHPVIZTKLoviOmCtW8560v%2FS%2FJxaJFLgZGhAf%2Fg%2BF%2FfX1B9%2Bn0VMSdlG5oa%2Fh6VE%2FWdp0Mq3%2F2oqE9nz0fSO5Jwp4VjZGZCP8HWFGnLN%2BLG3ERo9AiEKDDcKUm%2BN00Vf4ua1a%2Fc9qm18sq68cA%3D%3D",sep="\t",names=['liked','review'])
 
 stopset = set(stopwords.words('english'))
 vectorizer = TfidfVectorizer(use_idf = True, lowercase = True, strip_accents = 'ascii', stop_words = stopset)
